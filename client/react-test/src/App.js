4e66ede1215c8c1c1d9b0761c38ffe7f532b4993
shj0000 <shj0000@ex-it.co.kr>
2021-11-24 13:00:14 +0900

    init client

--- client/react-test/src/App.js
+++ client/react-test/src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Main from './component/Main';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>---
-        </p>
-		<textarea></textarea>
-
-		<select name="order" form="myForm">
-			<option value="americano">아메리카노</option>
-			<option value="caffe latte">카페라테</option>
-			<option value="cafe au lait">카페오레</option>
-			<option value="espresso">에스프레소</option>
-		</select>
-		<button>exec</button>
-		<textarea></textarea>
-      </header>
+      <Main />
     </div>
   );
 }
---
+++ client/react-test/src/component/Main.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  static getDerivedStateFromProps(props, state) {
+    return {favoritecolor: props.favcol };
+  }
+  
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({favoritecolor: "yellow"})
+    }, 1000)
+    console.log("test");
+  }
+
+  render() {
+    return <h1>Hello, 22{this.props.name}</h1>;
+  }
+}
+
+export default Main;
