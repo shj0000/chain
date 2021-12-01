530f17ec62b997de2fdca1f8bef4c9e52fade391
shj0000 <89052067+shj0000@users.noreply.github.com>
2021-12-02 00:25:52 +0900

    Update App.js

--- client/react-test2/src/App.js
+++ client/react-test2/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>---
-        </p> haha 222
+        </p> haha 222 333
 		<textarea></textarea>
 		dddd
 		<select name="order" form="myForm">
