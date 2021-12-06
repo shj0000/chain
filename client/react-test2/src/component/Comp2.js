96dc0e9dceb93651d8555c470892ebef44ec3999
shj0000 <89052067+shj0000@users.noreply.github.com>
2021-12-06 12:55:56 +0900

    Update Comp2.js

--- client/react-test2/src/component/Comp2.js
+++ client/react-test2/src/component/Comp2.js
@@ -19,7 +19,7 @@ class Comp2 extends React.Component {
   }
   
   render() {
-    return <h1 onClick={this.test}>Button ㅅ{this.props.name}</h1>;
+    return <h1 onClick={this.test}>Button - Send - Account Info{this.props.name}</h1>;
   }
 }
 
