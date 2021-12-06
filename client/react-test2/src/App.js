9ccad0aafa2bf6df52f59480cbb0196141274d59
shj0000 <89052067+shj0000@users.noreply.github.com>
2021-12-06 12:48:37 +0900

    Update App.js

--- client/react-test2/src/App.js
+++ client/react-test2/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Main from './component/Main';
+import Comp1 from './component/Comp1';
 
 function App() {
   return (
     <div className="App">
       <Main />
+      <Comp1 />
     </div>
   );
 }
