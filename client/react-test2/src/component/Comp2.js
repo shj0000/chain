de6c620006cb0606c3ae3974366aa254f51e62d0
shj0000 <89052067+shj0000@users.noreply.github.com>
2021-12-06 12:49:36 +0900

    Create Comp2.js

---
+++ client/react-test2/src/component/Comp2.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Comp2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  static getDerivedStateFromProps(props, state) {
+    return {favoritecolor: props.favcol };
+  }
+  
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({favoritecolor: "yellow"})
+    }, 1000)
+    console.log("test");
+  }
+
+  render() {
+    return <h1>Hello, 22 33{this.props.name}</h1>;
+  }
+}
+
+export default Comp2;
