1aea4a859da78b02e2ca9b28794ad17865802ac5
shj0000 <89052067+shj0000@users.noreply.github.com>
2021-12-21 12:39:58 +0900

    Update Comp2.js

--- client/react-test2/src/component/Comp2.js
+++ client/react-test2/src/component/Comp2.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import * as solanaWeb3 from '@solana/web3.js';
+const {struct, u32, ns64} = require("@solana/buffer-layout");
+const {Buffer} = require('buffer');
+const web3 = require("@solana/web3.js");
 
 class Comp2 extends React.Component {
   constructor(props) {
@@ -18,7 +20,45 @@ class Comp2 extends React.Component {
   test() {
     alert(2255);
     console.log("solanaWeb3");
-    console.log(solanaWeb3);
+    
+    
+    let keypair = web3.Keypair.generate();
+    let payer = web3.Keypair.generate();
+
+    let connection = new web3.Connection(web3.clusterApiUrl('testnet'));
+
+    let airdropSignature = await connection.requestAirdrop(
+      payer.publicKey,
+      web3.LAMPORTS_PER_SOL,
+    );
+
+    await connection.confirmTransaction(airdropSignature);
+
+    let allocateTransaction = new web3.Transaction({
+      feePayer: payer.publicKey
+    });
+    let keys = [{pubkey: keypair.publicKey, isSigner: true, isWritable: true}];
+    let params = { space: 100 };
+
+    let allocateStruct = {
+      index: 8,
+      layout: struct([
+        u32('instruction'),
+        ns64('space'),
+      ])
+    };
+
+    let data = Buffer.alloc(allocateStruct.layout.span);
+    let layoutFields = Object.assign({instruction: allocateStruct.index}, params);
+    allocateStruct.layout.encode(layoutFields, data);
+
+    allocateTransaction.add(new web3.TransactionInstruction({
+      keys,
+      programId: web3.SystemProgram.programId,
+      data,
+    }));
+
+    await web3.sendAndConfirmTransaction(connection, allocateTransaction, [payer, keypair]);
   }
   
   render() {
