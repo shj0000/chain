833ccd87f2afe1afa59acf7cf423cefca0f40716
ganaab0000 <ganaab0000@gmail.com>
2021-12-26 02:02:43 +0900

    프로젝트 추가 구성

---
+++ client/react-ranking/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Main from './component/Main';
+import Comp1 from './component/Comp1';
+import Comp2 from './component/Comp2';
+import Comp3 from './component/Comp3';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+      <Comp1 />
+      <Comp2 />
+      <Comp3 />
+    </div>
+  );
+}
+
+export default App;
