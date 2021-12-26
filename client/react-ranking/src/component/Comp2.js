7871c53064bf604b288e776934b58fd574c03772
ganaab0000 <ganaab0000@gmail.com>
2021-12-26 16:41:20 +0900

    솔라나 예제 코드 초기 구성

--- client/react-ranking/src/component/Comp2.js
+++ client/react-ranking/src/component/Comp2.js
@@ -1,9 +1,43 @@
-import React from 'react';
-const {struct, u32, ns64} = require("@solana/buffer-layout");
-const {Buffer} = require('buffer');
-const web3 = require("@solana/web3.js");
+
+import React, { useEffect, useState } from "react";
+
+import * as solanaWeb3 from "@solana/web3.js";
+import {
+  Keypair,
+  clusterApiUrl,
+  Commitment,
+  Connection,
+  RpcResponseAndContext,
+  SignatureStatus,
+  SimulatedTransactionResponse,
+  Transaction,
+  TransactionInstruction,
+  TransactionSignature,
+  Blockhash,
+  FeeCalculator,
+  PublicKey
+} from "@solana/web3.js";
+
+import {
+  TokenInfo,
+  TokenListProvider,
+  ENV as ChainId
+} from "@solana/spl-token-registry";
+import * as borsh from "borsh";
+
+import {
+  AccountAndPubkey,
+  Metadata,
+  METADATA_SCHEMA
+} from "./processMaetaplexAccounts";
+import { deserializeMint, deserializeAccount } from "./deserialize";
+
+// const {struct, u32, ns64} = require("@solana/buffer-layout");
+// const {Buffer} = require('buffer');
+// const web3 = require("@solana/web3.js");
 
 class Comp2 extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {favoritecolor: "red"};
@@ -17,52 +51,171 @@ class Comp2 extends React.Component {
     }, 1000)
     console.log("test");
   }
-  async test() {
-    alert(2255);
+
+  async test(e) {
     console.log("solanaWeb3");
-    
-    
-    let keypair = web3.Keypair.generate();
-    let payer = web3.Keypair.generate();
+    console.log(solanaWeb3);
 
-    let connection = new web3.Connection(web3.clusterApiUrl('testnet'));
+    const METADATA_REPLACE = new RegExp("\u0000", "g");
+    function decodeMetadata(buffer) {
+      console.log("check");
+      // return borsh.deserializeUnchecked(METADATA_SCHEMA, Metadata, buffer);
 
-    let airdropSignature = await connection.requestAirdrop(
-      payer.publicKey,
-      web3.LAMPORTS_PER_SOL,
+      const metadata = borsh.deserializeUnchecked(
+        METADATA_SCHEMA,
+        Metadata,
+        buffer
+      );
+      metadata.data.name = metadata.data.name.replace(METADATA_REPLACE, "");
+      metadata.data.uri = metadata.data.uri.replace(METADATA_REPLACE, "");
+      metadata.data.symbol = metadata.data.symbol.replace(METADATA_REPLACE, "");
+      return metadata;
+    }
+
+    const ENDPOINTS = [
+      {
+        name: "mainnet-beta",
+        endpoint: "https://api.metaplex.solana.com/",
+        ChainId: ChainId.MainnetBeta
+      },
+      {
+        name: "mainnet-beta (Solana)",
+        endpoint: "https://api.mainnet-beta.solana.com",
+        ChainId: ChainId.MainnetBeta
+      },
+      {
+        name: "mainnet-beta (Serum)",
+        endpoint: "https://solana-api.projectserum.com/",
+        ChainId: ChainId.MainnetBeta
+      },
+      {
+        name: "testnet",
+        endpoint: clusterApiUrl("testnet"),
+        ChainId: ChainId.Testnet
+      },
+      {
+        name: "devnet",
+        endpoint: clusterApiUrl("devnet"),
+        ChainId: ChainId.Devnet
+      }
+    ];
+
+    const DEFAULT = ENDPOINTS[0].endpoint;
+    const conn = new Connection(DEFAULT, "recent");
+
+    const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
+      "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+    );
+    //
+    const creatorAddress = new PublicKey(
+      "2KgSbEGLo64knHru5CDsf3Gsf5WjVXXr7a9oiaakxEzb"
+    );
+    //const mintAddress = "Hu8k9rabgTxqcwM95o2ng4UQmxakepZ4w2YLJMTW4F3u";
+    //2KgSbEGLo64knHru5CDsf3Gsf5WjVXXr7a9oiaakxEzb
+    //Hu8k9rabgTxqcwM95o2ng4UQmxakepZ4w2YLJMTW4F3u
+    alert(3);
+    const MAX_NAME_LENGTH = 32;
+    const MAX_URI_LENGTH = 200;
+    const MAX_SYMBOL_LENGTH = 10;
+    const MAX_CREATOR_LEN = 32 + 1 + 1;
+    const TOKEN_PROGRAM_ID = new PublicKey(
+      "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
     );
 
-    await connection.confirmTransaction(airdropSignature);
+    //owner
+    const account = conn
+      .getTokenAccountsByOwner(creatorAddress, {
+        programId: TOKEN_PROGRAM_ID
+      })
+      .then((el0) => {
+        console.log("el0", el0);
+        el0.value.forEach((el00) => {
+          // pubkey = Token Account:Address
+          // maybe data=mint
+          // console.log(el00.pubkey.toBase58());
+          //deserializeAccount
+          //deserializeMint
+          console.log(deserializeAccount(el00.account.data));
+          console.log(
+            new PublicKey(deserializeAccount(el00.account.data).mint).toString()
+          );
+          //Buffer.from(el00.account.data, "base64")
+        });
+      });
 
-    let allocateTransaction = new web3.Transaction({
-      feePayer: payer.publicKey
-    });
-    let keys = [{pubkey: keypair.publicKey, isSigner: true, isWritable: true}];
-    let params = { space: 100 };
+    conn
+      .getProgramAccounts(
+        TOKEN_METADATA_PROGRAM_ID,
 
-    let allocateStruct = {
-      index: 8,
-      layout: struct([
-        u32('instruction'),
-        ns64('space'),
-      ])
-    };
+        {
+          filters: [
+            {
+              memcmp: {
+                offset:
+                  1 + // key
+                  32 + // update auth
+                  32 + // mint
+                  4 + // name string length
+                  MAX_NAME_LENGTH + // name
+                  4 + // uri string length
+                  MAX_URI_LENGTH + // uri*
+                  4 + // symbol string length
+                  MAX_SYMBOL_LENGTH + // symbol
+                  2 + // seller fee basis points
+                  1 + // whether or not there is a creators vec
+                  4 + // creators vec length
+                  0 * MAX_CREATOR_LEN,
+                bytes: creatorAddress.toBase58()
+              }
+            }
+          ]
+        }
+      )
+      .then((el) => {
+        console.log("el1", el);
+        console.log("el11", el[0].account.data.toString());
 
-    let data = Buffer.alloc(allocateStruct.layout.span);
-    let layoutFields = Object.assign({instruction: allocateStruct.index}, params);
-    allocateStruct.layout.encode(layoutFields, data);
+        console.log(decodeMetadata(el[0].account.data));
+        var res = decodeMetadata(el[0].account.data);
+        var resAddr = res.data.creators[0].address;
+        var pubKey = new PublicKey(resAddr);
+        console.log("###", pubKey.toString());
+        console.log("######");
+        // decodeMetadata(el[0].account.data.toString()).then((reEl) =>
+        //   console.log("el111", reEl)
+        // );
+      });
 
-    allocateTransaction.add(new web3.TransactionInstruction({
-      keys,
-      programId: web3.SystemProgram.programId,
-      data,
-    }));
-    console.log(
-      await web3.sendAndConfirmTransaction(connection, allocateTransaction, [payer, keypair])
+    const mintAddress = new PublicKey(
+      "kz2UZ6WBqg1KzfyYZUFh7mJkApa8MmUZDuGNYpVLNZr"
     );
-    
-  }
-  
+    //4itgFt6tSotypyVAaUkLJzpGQ5KXsJNhwpKBANMv49mf
+    //DrMT4YPqc8FjSNQKLKHhWwh1fLL5j6Q51ezPurWZK9Dm
+    conn
+      .getProgramAccounts(
+        TOKEN_METADATA_PROGRAM_ID,
+
+        {
+          filters: [
+            {
+              memcmp: {
+                offset: 1 + 32, // key // update auth
+                bytes: mintAddress.toString()
+              }
+            }
+          ]
+        }
+      )
+      .then((el) => {
+        console.log("el2", el);
+        console.log("el22", el[0].account.data.toString());
+      });
+
+    const newValue = `${e.target.value}`.trim().toLowerCase();
+
+  };
+
+
   render() {
     return <h1 onClick={this.test}>Button - Send - Account Info{this.props.name}</h1>;
   }
