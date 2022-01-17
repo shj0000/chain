7e88d9504a7b970d2025a512db6347ac46ba6a82
shj0000 <shj0000@ex-it.co.kr>
2022-01-16 22:39:44 -0500

    rpc api server - rename

---
+++ rpc_api_server_basic/README.md
@@ -0,0 +1,2 @@
+# rpc-api-server
+* test
---
+++ rpc_api_server_basic/test_git.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ~
+
+git clone https://github.com/shj0000/chain.git
+
+cd chain/
+
+ls -alF
+
+git checkout dev
+
+cd docs
+
+bash test.sh
