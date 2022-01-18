ed79e153fb024d342887e3b9bb40e65764356671
shj0000 <89052067+shj0000@users.noreply.github.com>
2022-01-18 13:08:50 +0900

    Update stream_server.js

--- rpc_api_server_basic/stream_server.js
+++ rpc_api_server_basic/stream_server.js
@@ -32,6 +32,29 @@ app.get('/test', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/restart', (req, res) => {
+  res.send('Hello World! Restart!');
+  var spawn = require('child_process').spawn;
+
+  (function main() {
+
+    if (process.env.process_restarting) {
+      delete process.env.process_restarting;
+      // Give old process one second to shut down before continuing ...
+      setTimeout(main, 1000);
+      return;
+    }
+
+    // ...
+
+    // Restart process ...
+    spawn(process.argv[0], process.argv.slice(1), {
+      env: { process_restarting: 1 },
+      stdio: 'ignore'
+    }).unref();
+  })();
+});
+
 app.get('/git/pull', (req, res) => {
   res.send('Hello World!');
   const path = '../shell_script/git_pull.sh';
