dd52f1b309a4e3bf420f3c79f655be7182264882
shj0000 <89052067+shj0000@users.noreply.github.com>
2022-01-18 12:41:13 +0900

    Update stream_server.js

--- rpc_api_server_basic/stream_server.js
+++ rpc_api_server_basic/stream_server.js
@@ -1,34 +1,44 @@
 const { spawn } = require("child_process");
-
-
-
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+app.get('/test', (req, res) => 
+  res.send('Hello World!');
+});
+
+app.get('/git/pull', (req, res) => 
+  res.send('Hello World!');
+});
+
+app.get('/exec/shell_script/test', (req, res) => 
+  res.send('Hello World!');
+        
+  // ls -> process
+  const ls = spawn('bash', ['./test_git.sh']);
+
+  ls.stdout.on("data", data => {
+      console.log(`stdout: ${data}`);
+  });
+
+  ls.stderr.on("data", data => {
+      console.log(`stderr: ${data}`);
+  });
+
+  ls.on('error', (error) => {
+      console.log(`error: ${error.message}`);
+  });
+
+  ls.on("close", code => {
+      console.log(`child process exited with code ${code}`);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
 
-// ls -> process
-const ls = spawn('bash', ['./test_git.sh']);
-
-ls.stdout.on("data", data => {
-    console.log(`stdout: ${data}`);
-});
-
-ls.stderr.on("data", data => {
-    console.log(`stderr: ${data}`);
-});
-
-ls.on('error', (error) => {
-    console.log(`error: ${error.message}`);
-});
-
-ls.on("close", code => {
-    console.log(`child process exited with code ${code}`);
-});
