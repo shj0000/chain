9e790bd8c11a1958261fda9a1a23cc1bc621aca2
shj0000 <89052067+shj0000@users.noreply.github.com>
2022-01-18 13:02:17 +0900

    Update stream_server.js

--- rpc_api_server_basic/stream_server.js
+++ rpc_api_server_basic/stream_server.js
@@ -3,6 +3,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+function cmd(path) {
+  const process = spawn('bash', [path]);
+
+  process.stdout.on("data", data => {
+      console.log(`stdout: ${data}`);
+  });
+
+  process.stderr.on("data", data => {
+      console.log(`stderr: ${data}`);
+  });
+
+  process.on('error', (error) => {
+      console.log(`error: ${error.message}`);
+  });
+
+  process.on("close", code => {
+      console.log(`child process exited with code ${code}`);
+  });
+};
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
@@ -13,29 +34,16 @@ app.get('/test', (req, res) => {
 
 app.get('/git/pull', (req, res) => {
   res.send('Hello World!');
+  const path = '../shell_script/git_pull.sh';
+  cmd(path);
 });
+
+// server restart
 
 app.get('/exec/shell_script/test', (req, res) => {
   res.send('Hello World!');
-        
-  // ls -> process
-  const ls = spawn('bash', ['./test.sh']);
-
-  ls.stdout.on("data", data => {
-      console.log(`stdout: ${data}`);
-  });
-
-  ls.stderr.on("data", data => {
-      console.log(`stderr: ${data}`);
-  });
-
-  ls.on('error', (error) => {
-      console.log(`error: ${error.message}`);
-  });
-
-  ls.on("close", code => {
-      console.log(`child process exited with code ${code}`);
-  });
+  const path = '../shell_script/test.sh';
+  cmd(path);
 });
 
 app.get('/exec/shell_script/param', (req, res) => {
@@ -45,24 +53,9 @@ app.get('/exec/shell_script/param', (req, res) => {
     path: ""
   };
   
-  // ls -> process
-  const ls = spawn('bash', ['./test_git.sh']);
-
-  ls.stdout.on("data", data => {
-      console.log(`stdout: ${data}`);
-  });
-
-  ls.stderr.on("data", data => {
-      console.log(`stderr: ${data}`);
-  });
-
-  ls.on('error', (error) => {
-      console.log(`error: ${error.message}`);
-  });
-
-  ls.on("close", code => {
-      console.log(`child process exited with code ${code}`);
-  });
+  const path = '../shell_script/test_git.sh';
+  cmd(path);
+  
 });
 
 app.listen(port, () => {
