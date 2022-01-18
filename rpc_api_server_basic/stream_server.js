7bbc8fd2ce0d3187ac30dfeeb9f29287b0a20652
shj0000 <89052067+shj0000@users.noreply.github.com>
2022-01-18 12:45:54 +0900

    Update stream_server.js

--- rpc_api_server_basic/stream_server.js
+++ rpc_api_server_basic/stream_server.js
@@ -19,6 +19,33 @@ app.get('/exec/shell_script/test', (req, res) =>
   res.send('Hello World!');
         
   // ls -> process
+  const ls = spawn('bash', ['./test.sh']);
+
+  ls.stdout.on("data", data => {
+      console.log(`stdout: ${data}`);
+  });
+
+  ls.stderr.on("data", data => {
+      console.log(`stderr: ${data}`);
+  });
+
+  ls.on('error', (error) => {
+      console.log(`error: ${error.message}`);
+  });
+
+  ls.on("close", code => {
+      console.log(`child process exited with code ${code}`);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
+
+app.get('/exec/shell_script/test/2', (req, res) => 
+  res.send('Hello World!');
+        
+  // ls -> process
   const ls = spawn('bash', ['./test_git.sh']);
 
   ls.stdout.on("data", data => {
