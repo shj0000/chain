13ab6cdb8f923667a514b95f0310ac78e4d0c21d
shj0000 <shj0000@ex-it.co.kr>
2022-01-25 13:06:38 +0900

    Edit rpc client

--- rpc_api_client_basic/rpc-react/src/component/Comp7.js
+++ rpc_api_client_basic/rpc-react/src/component/Comp7.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import io from "socket.io-client";   //모듈 가져오기
+const socketUrl = 'http://192.168.110.88:10011/';
+const socket = io(socketUrl);  //3001번 포트 사용(서버)
 
 class Comp7 extends React.Component {
   constructor(props) {
@@ -11,6 +14,27 @@ class Comp7 extends React.Component {
   }
   
   componentDidMount() {
+	// 서버로 자신의 정보를 전송한다.
+	socket.emit("login", {
+	  // name: "ungmo2",
+	  name: makeRandomName(),
+	  userid: "ungmo2@gmail.com"
+	});
+
+	// 서버로부터의 메시지가 수신되면
+	socket.on("login", function(data) {
+		this.setState({ output: data });
+	});
+	
+	function makeRandomName(){
+	  var name = "";
+	  var possible = "abcdefghijklmnopqrstuvwxyz";
+	  for( var i = 0; i < 3; i++ ) {
+		name += possible.charAt(Math.floor(Math.random() * possible.length));
+	  }
+	  return name;
+	}
+		
   }
   
   calc = () => {
@@ -44,25 +68,26 @@ class Comp7 extends React.Component {
 	let defaultInputData = {
 		urlPath: 'urlPath',
 	};
+	
 	function sendFunc(obj, config){
 		const inputData = obj.state.input;
-		// alert(config.defaultInputData.urlPath);
-		// https://jasonwatmore.com/post/2020/02/01/react-fetch-http-post-request-examples
-		// Simple POST request with a JSON body using fetch
-		const requestOptions = {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ title: 'React POST Request Example' })
-		};
-		obj.setState({ output: 'loading...' })
-		fetch('https://reqres.in/api/posts', requestOptions)
-			.then(response => response.json())
-			.then(data => obj.setState({ output: data.id }));
+		
+		// 서버로부터의 메시지가 수신되면
+		socket.on("chat", function(data) {
+			obj.setState({ output: data });
+		});
+
+		// Send 버튼이 클릭되면
+		socket.emit("chat", { msg: inputData });
+		// 서버로 메시지를 전송한다.
+		
 	};
+	
 	let config = {
 		defaultInputData: defaultInputData,
 		sendFunc: sendFunc,
 	};
+	
 	config.sendFunc(this, config);
   }
   
@@ -84,7 +109,7 @@ class Comp7 extends React.Component {
 			<div>
 				<textarea value={this.state.input} onChange={this.onChangeTxt}/>
 				<hr/>
-				<button onClick={this.calc}>calc</button>
+				<button onClick={this.calcWs}>calcWs</button>
 				<hr/>
 				<textarea value={this.state.output} onChange={this.onChangeTxt}/>
 			</div>
