b7d55d78ee37bd716400bf477195ddffef85fccf
shj0000 <89052067+shj0000@users.noreply.github.com>
2022-02-15 12:57:37 +0900

    Update install_solana.sh

--- shell_script/install_solana.sh
+++ shell_script/install_solana.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+# path
 cd ~
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# install solana
+sh -c "$(curl -sSfL https://release.solana.com/v1.9.6/install)"
+
+# check
+solana --version
