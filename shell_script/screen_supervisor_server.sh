962ee84a04e4089c6efaf403ff4cad619225a56a
shj0000 <89052067+shj0000@users.noreply.github.com>
2022-02-17 16:34:43 +0900

    Update screen_supervisor_server.sh

--- shell_script/screen_supervisor_server.sh
+++ shell_script/screen_supervisor_server.sh
@@ -5,7 +5,7 @@
 screen -ls
 screen -S screen_session_name_supervisor_server -X quit
 screen -ls
-screen -dmS screen_session_name_supervisor_server bash -c 'cd ~/chain/rpc_api_server_basic; supervisor ws_server.js'
+screen -dmS screen_session_name_supervisor_server bash -c 'cd ~/chain/rpc_api_server_basic; npm install; supervisor ws_server.js'
 screen -ls
 
 # 실행(있을 경우)
