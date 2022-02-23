c530298cefa4593d7503085e202d24e107eee5ca
shj0000 <shj0000@ex-it.co.kr>
2022-02-23 13:29:25 +0900

    edit client for TODO comment

--- rpc_api_client_basic/rpc-react/src/component/Comp7.js
+++ rpc_api_client_basic/rpc-react/src/component/Comp7.js
@@ -9,6 +9,23 @@ import Mousetrap from 'mousetrap';
 const socketUrl = 'http://192.168.110.88:10011/';
 const socket = io(socketUrl);  //3001번 포트 사용(서버)
 
+		// TODO :
+		// tab으로 치면 url 리스트에서 다음 단어 검색 출력하는 단일 메소드 구현
+		// enter는 각 메소드에서 구현 상황에 따라 1. 변수 부족 시 디폴트 데이터 출력 혹은 2. 웹 결과 출력 3. 웹소켓 연결 위한 결과 출력
+		// 3번 케이스일 경우, 연결 시 await를 통해 바로 웹소켓 전송.
+		// TODO :
+		// 웹서버 콜백함수로 데코레이터 패턴 로깅 적용.
+		// ... URL 등. 환경 에 따른 ON/OFF
+		
+	// TODO
+	// https://ourcodeworld.com/articles/read/309/top-5-best-code-editor-plugins-written-in-javascript
+	// input tab issue
+	// https://gusrb3164.github.io/web/2021/01/07/react-ace/
+	// Ctrl + H
+	// 단축키 설명 Modal
+	// alt + 1,2,3
+	// 크기 조정 해당 창 제외 한줄 렌더링
+	
 class Comp7 extends React.Component {
 
 	constructor(props) {
@@ -51,12 +68,6 @@ class Comp7 extends React.Component {
 	}
 	
 	componentDidMount() { 
-	// TODO
-	// https://ourcodeworld.com/articles/read/309/top-5-best-code-editor-plugins-written-in-javascript
-	// input tab issue
-	// https://gusrb3164.github.io/web/2021/01/07/react-ace/
-	// Ctrl + H
-	// 단축키 설명 Modal
 		
 		Mousetrap.bind("alt+1", () => {
 		  this.focusTextInput(1)
@@ -191,14 +202,6 @@ class Comp7 extends React.Component {
 
 
 	convertCliToSend = (input) => {
-		// TODO :
-		// tab으로 치면 url 리스트에서 다음 단어 검색 출력하는 단일 메소드 구현
-		// enter는 각 메소드에서 구현 상황에 따라 1. 변수 부족 시 디폴트 데이터 출력 혹은 2. 웹 결과 출력 3. 웹소켓 연결 위한 결과 출력
-		// 3번 케이스일 경우, 연결 시 await를 통해 바로 웹소켓 전송.
-		// TODO :
-		// 웹서버 콜백함수로 데코레이터 패턴 로깅 적용.
-		// ... URL 등. 환경 에 따른 ON/OFF
-		
 
 		// var input = ` ws   asd   www  -d a d  -d 'dfad' -w "dafadf" -df adfwd --dfef 'dff'   'text' "text" text 'text' "text"          `;
 		input = this.state.input;
